7c2558acae91c2125853653d038808bb8643047c
Sid <siddharthkpandey@gmail.com>
2017-08-21 23:27:10 +0100

    Initial Commit

---
+++ AzureRedisPlayArea/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureRedisPlayArea.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace AzureRedisPlayArea.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDistributedCache _cache;
+
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            Audit audit = JsonConvert.DeserializeObject<Audit>(_cache.GetString("Audit") ?? "");
+
+            if (audit == null)
+            {
+                audit = new Audit() { Name = "Important Audit", LastUpdated = DateTime.Now };
+
+                var options = new DistributedCacheEntryOptions();
+                options.SetSlidingExpiration(TimeSpan.FromSeconds(30));
+
+                _cache.SetString("Audit", JsonConvert.SerializeObject(audit), options);
+            }
+
+            ViewData["Audit"] = audit;
+
+            ViewData["DateTime"] = DateTime.Now.ToString(CultureInfo.CurrentCulture);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AzureRedisPlayArea/Models/Audit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureRedisPlayArea.Models
+{
+    public class Audit
+    {
+        public string Name { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
